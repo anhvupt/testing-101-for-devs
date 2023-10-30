ae2af1e7bda0c090b1aa07a798065b27949c2718
vu.pham <vu.pham@impartner.com>
2023-10-30 21:28:26 +0700

    init server

---
+++ controllers/task.controller.js
@@ -0,0 +1,63 @@
+// controllers/taskController.js
+
+// In-memory task data
+let tasks = [];
+
+// Create a new task
+const createTask = (req, res) => {
+    const newTask = req.body;
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+};
+
+// Read all tasks
+const getAllTasks = (req, res) => {
+    res.status(200).json(tasks);
+};
+
+// Read a specific task by ID
+const getTaskById = (req, res) => {
+    const taskId = req.params.taskId;
+    const task = tasks.find((t) => t.id === taskId);
+
+    if (task) {
+        res.status(200).json(task);
+    } else {
+        res.status(404).json({ message: 'Task not found' });
+    }
+};
+
+// Update a task by ID
+const updateTaskById = (req, res) => {
+    const taskId = req.params.taskId;
+    const updatedTask = req.body;
+    const index = tasks.findIndex((t) => t.id === taskId);
+
+    if (index !== -1) {
+        tasks[index] = updatedTask;
+        res.status(200).json(updatedTask);
+    } else {
+        res.status(404).json({ message: 'Task not found' });
+    }
+};
+
+// Delete a task by ID
+const deleteTaskById = (req, res) => {
+    const taskId = req.params.taskId;
+    const index = tasks.findIndex((t) => t.id === taskId);
+
+    if (index !== -1) {
+        tasks.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).json({ message: 'Task not found' });
+    }
+};
+
+module.exports = {
+    createTask,
+    getAllTasks,
+    getTaskById,
+    updateTaskById,
+    deleteTaskById,
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const taskController = require('./controllers/task.controller'); // Import the controller
+
+app.use(bodyParser.json());
+
+// Define your routes for CRUD operations using the controller functions
+
+app.post('/tasks', taskController.createTask);
+app.get('/tasks', taskController.getAllTasks);
+app.get('/tasks/:taskId', taskController.getTaskById);
+app.put('/tasks/:taskId', taskController.updateTaskById);
+app.delete('/tasks/:taskId', taskController.deleteTaskById);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
